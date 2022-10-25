cc440d8168f564379c7d5e41a50874343381614b
MK <mia.rfkb@mail.ru>
2022-10-25 20:48:10 +0300

    Commmit hometask 3, part 2

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        ObjectCat Cat1 = new ObjectCat();
+
+        Cat1.catName = "Marsik";
+        Cat1.catAge = 7;
+        Cat1.catColor = "red";
+        Cat1.catWeight = 5.5;
+
+        System.out.println(Cat1.toString());
+        System.out.println();
+    }
+}
---
+++ ObjectCat.java
@@ -0,0 +1,14 @@
+public class ObjectCat {
+    String catName;
+    int catAge;
+    String catColor;
+    double catWeight;
+
+    public void Cat(String catName, int catAge, String catColor, double catWeight) {
+    }
+    public String toString() {
+        String returnString = "My cat's name is " + catName + ". " + "He is " + catAge + ". " + "He is " + catColor + ". " + "He weights " + catWeight + " kilos.";
+        return returnString;
+    }
+
+}
